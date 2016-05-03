aef4cdbb330a31774e2be09616e1eca0315c5d08
Harrison Powers <harrisonpowers@gmail.com>
2016-05-03 16:02:47 -0400

    Add required and search text change

--- src/material-decorator.js
+++ src/material-decorator.js
@@ -102,9 +102,6 @@
       var title = args.form.title || args.form.placeholder || args.form.key.slice(-1)[0];
 
       if (mdAutocompleteFrag) {
-        if (args.form.onChange) {
-          mdAutocompleteFrag.setAttribute('md-selected-item-change', "evalExpr(form.onChange+'(\"'+item+'\")')");
-        }
         // mdAutocompleteFrag.setAttribute('md-items', 'item in $filter(''autocomplete'')(searchText);');
         mdAutocompleteFrag.setAttribute('md-min-length', minLength);
         if (maxLength) {
